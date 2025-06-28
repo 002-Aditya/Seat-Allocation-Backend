9947778447e3b0d15ef45a0b9f91cf7d82c9b220
Aditya-mac <dr.adityakumar2001@gmail.com>
2025-06-29 00:59:48 +0530

    log path modified including loggers as well

--- utils/logger.js
+++ utils/logger.js
@@ -1,17 +1,31 @@
+const path = require('path');
 const winston = require('winston');
 const DailyRotateFile = require('winston-daily-rotate-file');
+const { format } = require('winston');
+
+const logDir = path.join(__dirname, '../logs');
+
+// Custom filter to exclude 'error' level logs
+const excludeErrors = format((info) => {
+    return info.level === 'error' ? false : info;
+});
 
 const normalLogTransport = new DailyRotateFile({
-    dirname: '/var/log/containers/',
+    dirname: logDir,
     filename: 'application-info-%DATE%.log',
     datePattern: 'YYYY-MM-DD',
     //   maxFiles: '5d',
     zippedArchive: false,
     // maxSize: '10m',
+    format: format.combine(
+        excludeErrors(),
+        format.timestamp(),
+        format.json()
+    ),
 });
 
 const errorLogTransport = new DailyRotateFile({
-    dirname: '/var/log/containers/',
+    dirname: logDir,
     filename: 'application-error-%DATE%.log',
     datePattern: 'YYYY-MM-DD',
     zippedArchive: false,
