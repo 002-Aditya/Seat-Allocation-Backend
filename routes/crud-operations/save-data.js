141d9ff21e228def31308fce53b92e9ebdc68514
Aditya-mac <dr.adityakumar2001@gmail.com>
2025-07-14 15:28:59 +0530

    login and create user API not need token

--- routes/crud-operations/save-data.js
+++ routes/crud-operations/save-data.js
@@ -3,16 +3,22 @@ const router = express.Router();
 const isAuth = require('../../middleware/auth');
 const rowsArrangementController = require('../../controllers/rows-arrangement');
 const userController = require('../../controllers/user-details');
-const handleMethodRouting = require("../handle-method-routings");
+const handleMethodRouting = require('../handle-method-routings');
 
-// Controller to handle dynamic routing using a map
-const mappings = {
+const unauthenticatedMappings = {
     user: userController.createUser,
+};
+
+const authenticatedMappings = {
     rowsArrangement: rowsArrangementController.createRows,
 };
 
+router.post('/public', (req, res) => {
+    handleMethodRouting(req, res, unauthenticatedMappings);
+});
+
 router.post('/', isAuth, (req, res) => {
-    handleMethodRouting(req, res, mappings);
+    handleMethodRouting(req, res, authenticatedMappings);
 });
 
 module.exports = router;
